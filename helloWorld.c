6bbc76380fbe745422233dd7efeb2847f2d74614
acousticCh <hassenr@protonmail.com>
2024-10-18 21:00:53 -0400

    Start of learning C

---
+++ helloWorld.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+	// number variable for strings format specifier
+	int number = 101;
+	// outputs hello world
+	printf("hello world\n");
+	/*this outputs the string with the called variable
+	 using a format specifier*/
+	printf("This is my first c variable: %d\n\n", number);
+	// ends main function
+	return 0;
+}
---
+++ strings.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+	//string practice
+	//declare sting in varible
+	//strings are declared as character arrays
+	char myStr[] = "Hello World this is my first string in C";
+
+	/*printing strings uses format specifier %s
+	and no brackets when the variable is called */
+	printf("%s\n", myStr);
+
+	return 0;
+}
